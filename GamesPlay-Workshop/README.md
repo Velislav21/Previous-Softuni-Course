a11f3836532148ac6a85b6aa426d1b6a3da93dbc
Velislav21 <zirovcho34@gmail.com>
2024-12-30 14:37:41 +0200

    .
    main components separated.

--- GamesPlay-Workshop/README.md
+++ GamesPlay-Workshop/README.md
@@ -6,3 +6,10 @@ SoftUni React Course Project
 - [x] Add softuni practice server
 - [x] Add base vite react project as client
 - [x] Add project resources
+- [x] Separate html into components
+
+## 2. React Router
+- [x] Install react-router-dom
+- [x] Setup react-router-dom
+- [x] Add routes in App.jsx
+- [x] Add links in the navigation
--- GamesPlay-Workshop/client/src/App.jsx
+++ GamesPlay-Workshop/client/src/App.jsx
@@ -3,18 +3,21 @@ import { Route, Routes } from 'react-router-dom'
 import Header from './components/header/Header'
 import Login from './components/user/login/Login'
 import Register from './components/user/register/Register'
+import CreateGame from './components/create-edit-game/create/CreateGame'
+import AllGames from './components/games/AllGames'
 
 function App() {
 
   return (
     <div id="box">
-      <Header/>
+      <Header />
 
       <Routes>
 
-        <Route path={'/login'} element={<Login/>}></Route>
-        <Route path={'/register'} element={<Register/>}></Route>
-
+        <Route path={'/login'} element={<Login />} />
+        <Route path={'/register'} element={<Register />} />
+        <Route path={'/create'} element={<CreateGame />} />
+        <Route path={'/games'} element={<AllGames/>}/>
       </Routes>
     </div>
   )
