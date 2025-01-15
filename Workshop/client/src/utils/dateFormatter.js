ecde8b7e8ce1c180ad7299018d98c69f24a9144b
Velislav21 <zirovcho34@gmail.com>
2025-01-15 09:05:05 +0200

    .

--- Workshop/client/src/utils/dateFormatter.js
+++
@@ -1,5 +0,0 @@
-export function dateFormatter(dateFromInput) {
-    const date = new Date(dateFromInput);
-
-    return date.toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric'})
-}
